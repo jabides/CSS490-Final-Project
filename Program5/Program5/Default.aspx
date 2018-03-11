234d98c88a437f73ae2c3bd95f0bea92fc55efff
JJ Abides <jj.abides@hotmail.com>
2018-03-10 17:17:26 -0800

    Added markup and Azure Table setup

--- Program5/Program5/Default.aspx
+++ Program5/Program5/Default.aspx
@@ -4,26 +4,32 @@
 
     <div class="jumbotron">
         <h1>CSS 490 Program 5 by JJ Abides and Peter Stanton</h1>
-        <p class="lead">Welcome to our song artist viewer. Here you can input a song artist name and get data about them.</p>
+        <p class="lead">Welcome to our song artist viewer. Here you can input a song artist name and view their album and song data.</p>
         <br />
 
+        <p>Artist Name</p>
         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
 
         <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Enter" />
+
         <br />
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+        <br />
+        <p>Enter album</p>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+
+        <asp:Button ID="Button2" runat="server" Text="Query" />
+
     </div>
 
     <div class="row">
         <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
+            <asp:Label ID="ResultsTitle" Enabled="false" Font-Size="Large" runat="server">Results:</asp:Label>
+            <br />
+
+            <asp:Label ID="Results" runat="server"></asp:Label>
+          
         </div>
     </div>
 
--- Program5/Program5/Default.aspx.cs
+++ Program5/Program5/Default.aspx.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using SpotifyAPI.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Azure;
+using System.IO;
 
 namespace Program5
 {
@@ -11,6 +17,77 @@ namespace Program5
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Take the input and place it into a request
+            string input = TextBox1.Text;
+
+            if (input.Length == 0) return; //If the input is empty, don't do anything
+
+            ResultsTitle.Enabled = true;
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            CloudTable table = tableClient.GetTableReference("css490finalproject");
+
+
+            if (table.Exists()) //How do we update the table without deleting it?
+            {
+                table.Delete();
+            }
+
+            try
+            {
+                table.Create();
+            }
+            catch (Exception k)
+            {
+                ResultsTitle.Text = "The previous table is currently being deleted. Please wait a few seconds.";
+                return;
+            }
+
+
+
+            
+            
+
+            //Here we need to store each name into the table
+
+
+        }
+    }
+
+    public class UserData
+    {
+        public List<DynamicTableEntity> UserDataList;
+        public Dictionary<string, EntityProperty> data;
+        public DynamicTableEntity entity = null;
+
+        public UserData()
+        {
+            UserDataList = new List<DynamicTableEntity>();
+        }
+
+        public void addAttribute(string key, string value)
+        {
+            data.Add(key, new EntityProperty(value));
+        }
+
+        public void addEntityToDataList()
+        {
+            entity.Properties = data;
+            UserDataList.Add(entity);
+        }
+
+        public void createNewEntity(string partitionKey, string rowKey)
+        {
+            entity = new DynamicTableEntity();
+            data = new Dictionary<string, EntityProperty>();
+            entity.PartitionKey = partitionKey;
+            entity.RowKey = rowKey;
 
         }
     }
